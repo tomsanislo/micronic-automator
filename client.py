f23719b2f5f6c39e86375feff857e12eb500f28d
Tomáš Sanislo <tomsanislo@gmail.com>
2021-08-26 19:31:56 +0200

    Create client.py

---
+++ client.py
@@ -0,0 +1,12 @@
+# echo_client.py
+import socket
+
+host = '169.254.88.108'    
+port = 10005                   # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.sendall(str.encode('startcapping\r\n'))
+data = s.recv(1024)
+datadecode = data.decode('ascii')
+s.close()
+print('Received', str(datadecode))
